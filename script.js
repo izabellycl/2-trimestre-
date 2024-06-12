dab1c222c145c2a9944e97ffaf5728b8defce93b
izabellycl <117642132+izabellycl@users.noreply.github.com>
2024-06-12 07:58:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
+const caixaPrincipal=document.querySelector(."caixa-principal");
+const caixaPerguntas=document.querySelector(."caixa-perguntas");
+const caixaAlternativas=document.querySelector(."caixa-alternativas");
+const caixaResultado=document.querySelector(."caixa-resultado");
+const textoResultado=document.querySelector(."texto-resultado");
 
+const perguntas=[
+{
+enunciado: "Pergunta1",
+  alternativas:[
+    {
+texto:    "Alternativa1",
+afirmação: "afirmação", 
+    },
+  {
+    "Alternativa2",
+  ]
+}
+{
+enunciado: "quantas libertadores o palmeiras tem?",
+  alternativas:[
+    "3",
+    "2",
+  ]
+}
+{
+enunciado: "verde ou branco?",
+  alternativas:[
+    "branco",
+    "verde",
+  ]
+}
+  {
+enunciado: "cortinas são os menores do mundo?!",
+  alternativas:[
+    "sim",
+    "sim",
+  ]
+}
+    {
+enunciado: "Abel Ferreira é o melhor do mundo?!",
+  alternativas:[
+    "sim",
+    "com certeza",
+  ]
+}
+      ];
+
+    let atual=0;
+      let perguntaAtual;
+
+     funcion mostraPergunta(){
+       perguntaAtual=perguntas[atual];
+       caixaPerguntas.textosContent=perguntaAtual.enunciado;
+       mostraAlternativas();
+     }
+
+function mostraAlternativas(){
+for(const alternativa of perguntaAtual.alternativas){
+const botaoAlternativa = document.createElement("button");
+botaoAlternativas.textContent= alternativa;
+caixaAlternativas.appendChild(botaoAlternativa);
+}
+}
+    mostraPergunta();
